90f537e5ecb49335c54b84ff1c2c65f3ff06b276
Ivan Kaskatiyski <ikaskatiyski@gmail.com>
2023-04-27 16:28:58 +0300

    Create initial project configuration for build in library mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, './src/index.ts'),
+            name: 'use-identification-number',
+            fileName: 'use-identification-number',
+        },
+        rollupOptions: {
+            external: ['vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        },
+    },
     plugins: [vue()],
     resolve: {
         alias: {
