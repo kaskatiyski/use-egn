96e14fbab051fafb93a720698e2d8d970f790337
Ivan Kaskatiyski <ikaskatiyski@gmail.com>
2023-05-01 16:27:05 +0300

    Fix incorrect/missing typescript output files

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [vue(), dts()],
     build: {
         lib: {
             entry: path.resolve(__dirname, './src/index.ts'),
             name: 'use-egn',
-            fileName: 'use-egn',
+            fileName: (format) => `index.${format}.js`,
         },
         rollupOptions: {
             external: ['vue'],
@@ -22,7 +24,6 @@ export default defineConfig({
             },
         },
     },
-    plugins: [vue()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
